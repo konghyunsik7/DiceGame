35c0cd074375d51301060b5fd20c744c15651f95
Hyun Kong <konghyunsik7@gmail.com>
2022-07-22 23:34:17 -0400

    final

---
+++ DiceGame/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
+
+            Random random = new Random(); 
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+                Console.ReadKey();
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a "+ playerRandomNum);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("Enemy AI rolled a "+ enemyRandomNum);
+                
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("You have won this round!");
+                }
+                else if(playerRandomNum < enemyRandomNum)
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy has won this round!");
+                }
+                else
+                {
+                    Console.WriteLine("DRAW!");
+                }
+                Console.WriteLine($"The score is now - PLayer:{playerPoints}  Enemy:{enemyPoints}");
+                Console.WriteLine();
+            }
+
+            if(playerPoints > enemyPoints)
+            {
+                Console.WriteLine("You win!");
+            }    
+            else if (playerPoints< enemyPoints)
+            {
+                Console.WriteLine("Enemy wins");
+            }
+            else
+            {
+                Console.WriteLine("IT'S A DRAW");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
